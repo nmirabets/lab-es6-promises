6a337c78e9af519f4d6428cb5ccb9c374e52f72e
Nicolás <nmirabets@me.com>
2021-04-17 17:00:42 +0200

    Update script.js

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,31 +1,51 @@
 // This will print in the wrong order
 // we added it for you to test to make sure data is loaded
 // 🚨🚨🚨 comment out the next 3 lines when you start working on the code
-for (let i = 0; i < mashPotatoes.length; i++) {
-  //addFood(steak[i], '#steak');
-  console.log(mashPotatoes[i])
-}
+
+// for (let i = 0; i < mashPotatoes.length; i++) {
+//   //addFood(steak[i], '#steak');
+//   console.log(mashPotatoes[i])
+// }
 
 // Iteration 1 using callbacks
 addFood(steak[0], '#steak', () => {
-  // ... your code here
   addFood(steak[1], '#steak', () => {
-
+    addFood(steak[2], '#steak', () => {
+      addFood(steak[3], '#steak', () => {
+        addFood(steak[4], '#steak', () => {
+          addFood(steak[5], '#steak', () => {
+            addFood(steak[6], '#steak', () => {
+              addFood(steak[7], '#steak', () => {
+              })  
+            })  
+          })  
+        })  
+      })
+    })
   })
 });
 
-
-
 // Iteration 2 using `.then()`
-addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  // ... your code here
-  addFood(mashPotatoes[1], '#mashPotatoes')
+addFood(mashPotatoes[0], '#mashPotatoes').then((res) => {
+  addFood(mashPotatoes[1], '#mashPotatoes').then((res) => {
+    addFood(mashPotatoes[2], '#mashPotatoes').then((res) => {
+      addFood(mashPotatoes[3], '#mashPotatoes').then((res) => {
+        addFood(mashPotatoes[4], '#mashPotatoes').then((res) => {
+          
+        })
+      })
+    })
+  })
 });
 
 // Iteration 3 using async and await
 
   async function makeFood(step) {
-    // ... your code here
-    
+    return await addFood(brusselSprouts[step], '#brusselSprouts')
   }
-  makeFood(eachStep);
+
+  brusselSprouts.forEach((item, index) => {
+    makeFood(index)
+     });
+
+  //makeFood(eachStep);
